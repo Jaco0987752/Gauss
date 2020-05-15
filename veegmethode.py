f5b7818daee9387d6d0b7a8ef17d5521283f800d
Jaco0987752 <0987752@hr.nl>
2020-05-15 18:05:56 +0200

    Finished

--- veegmethode.py
+++ veegmethode.py
@@ -1,45 +1,86 @@
+# this code is written by Jaco de Jong.
 import numpy as np
 
 A = np.array([
     [2, -3, 5],
     [4, 5, -4],
     [6, 3, -2]
-])
+], dtype=float)
 
 B = np.array([[11],
               [2],
-              [6]])
+              [6]], dtype=float)
 
-def swep(A,B):
+def gauss(A,B):
     
     joined = np.concatenate((A,B), axis=1)
     print("joined")
     print(joined)
 
-    c = 0
-    for b in range( joined.shape[0]):
+    swep(joined)
+    print("swepped")
+    print(joined)
+
+    swap(joined)
+    print("swapped")
+    print(joined)
+
+    swep(joined, offset=1)
+    print("swepped again")
+    print(joined)
+
+    swap(joined)
+    print("swapped back")
+    print(joined)
+
+    divide(joined)
+    print("divided")
+    print(joined)
+
+    joined = np.around(joined,2)
+    print("rounded")
+    print(joined)
+    
+    columns = joined.shape[1]
+    rows = joined.shape[0]
+    result = [] 
+    for r in range(rows):
+            result.append(joined[r][columns-1])
+    return result
+
+        
+
+
+def swep(matrix, offset = 0):
+    for b in range( matrix.shape[0]):
         c = b-1
         if c<0: c = 0 
-        if b < joined.shape[0]-1: b = b+1
-        print(b,c)
-        factor = joined[b][c] / joined[c][c]
+        if b < matrix.shape[0]-1: b = b+1
+        factor = matrix[b][c+offset] / matrix[c][c+offset]
         print(factor)
-        for a in range(joined.shape[1]):
-            joined[b][a] = joined[b][a] - factor * joined[c][a]
-        print("joined after b= " + str(b) + "c=" + str(c))
-        print(joined)
+        for a in range(matrix.shape[1]):
+            matrix[b][a] = matrix[b][a] - factor * matrix[c][a]
 
+def swap(matrix):
+    toSwap = np.array(matrix, copy=True)
+    rows = matrix.shape[0]
+    columns = matrix.shape[1]
+    for r in range(rows):
+        for c in range(columns):
+            matrix[rows-1-r][columns-1-c ] = toSwap[r][c]
 
-    # factor = joined[2][0] / joined[0][0]
-    # for b in range(joined.shape[1]):
-    #     print(str(joined[2][a]) + " - " + str(factor) + " * " + str(joined[0][a])) 
-    #     joined[2][b] = joined[2][b] - factor * joined[0][b]
+def divide(matrix):
+    toSwap = np.array(matrix, copy=True)
+    rows = matrix.shape[0]
+    columns = matrix.shape[1]
+    for r in range(rows):
+        sum = 0
+        for c in range(columns -1):
+            sum += matrix[r][c]
+        matrix[r][columns-1] = matrix[r][columns-1]/sum
 
+            
 
-    #A[2][0] = A[2][0] - A[2][0] / A[0][0] * A[0][0]     
-
-    #A[2][1] = A[2][1] - A[2][1] / A[1][1] * A[1][1]
-
-    #A[2][2] = A[2][2] - A[2][2] / A[1][2] * A[1][2]
-
-swep(A,B)
+result = gauss(A,B)
+print("result")
+print(result)
